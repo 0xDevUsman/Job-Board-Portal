6f8d488ae397084f22d7ca68d9c9cd40d7ef05f0
0xDevUsman <ua1226925@gmail.com>
2025-05-26 14:45:46 +0530

    Edit Readme.md file

--- src/context/Authprovider.tsx
+++ src/context/Authprovider.tsx
@@ -4,3 +4,4 @@ import { SessionProvider } from "next-auth/react";
 export const Authprovider = ({ children }: { children: React.ReactNode }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
+
